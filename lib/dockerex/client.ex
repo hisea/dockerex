c46c7ee61a4ac9707e03d8f16b73cf763b12c01a
Yinghai Zhao <zyinghai@gmail.com>
2016-07-29 22:57:44 -0400

    update client.

--- lib/dockerex/client.ex
+++ lib/dockerex/client.ex
@@ -6,8 +6,8 @@ defmodule Docker.Client do
     "#{host}"
   end
 
-  defp options() do
-    
+  defp options do
+    options = Application.get_env(:docker, :opt)
   end
 
   @default_headers %{"Content-Type" => "application/json"}
@@ -15,31 +15,31 @@ defmodule Docker.Client do
   @doc """
   Send a GET request to the Docker API at the speicifed resource.
   """
-  def get(resource, headers \\ @default_headers) do
+  def get(resource, headers \\ @default_headers, opt \\ []) do
     Logger.debug "Sending GET request to the Docker HTTP API: #{resource}"
     base_url <> resource
-    |> HTTPoison.get!(headers)
+    |> HTTPoison.get!(headers, Keyword.merge(options, opt))
     |> decode_body
   end
 
   @doc """
   Send a POST request to the Docker API, JSONifying the passed in data.
   """
-  def post(resource, data \\ "", headers \\ @default_headers) do
+  def post(resource, data \\ "", headers \\ @default_headers, opt \\ []) do
     Logger.debug "Sending POST request to the Docker HTTP API: #{resource}, #{inspect data}"
     data = Poison.encode! data
     base_url <> resource
-    |> HTTPoison.post!(data, headers)
+    |> HTTPoison.post!(data, headers, Keyword.merge(options, opt))
     |> decode_body
   end
 
   @doc """
   Send a DELETE request to the Docker API.
   """
-  def delete(resource, headers \\ @default_headers) do
+  def delete(resource, headers \\ @default_headers, opt \\ []) do
     Logger.debug "Sending DELETE request to the Docker HTTP API: #{resource}"
     base_url <> resource
-    |> HTTPoison.delete!(headers)
+    |> HTTPoison.delete!(headers, Keyword.merge(options, opt))
   end
 
   defp decode_body(%HTTPoison.Response{body: ""}) do
